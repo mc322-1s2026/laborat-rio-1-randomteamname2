fa786137feaf183c89fb20f4dab5a313b81e87ed
viniciusalmeida-conpec <vinicius.almeida@conpec.com.br>
2026-03-24 02:03:24 -0300

    implementação de validação do email
    implementado a validação do email a partir do REGEX.
    
    OBS: mudança da assinatura dos getters

--- src/main/java/com/nexus/model/User.java
+++ src/main/java/com/nexus/model/User.java
@@ -1,16 +1,20 @@
 package com.nexus.model;
 
+import com.nexus.service.Workspace;
+import java.util.regex.Pattern;
+
 public class User {
     private final String username;
     private final String email;
 
     public User(String username, String email) {
+
         if (username == null || username.isBlank()) {
             throw new IllegalArgumentException("Username não pode ser vazio.");
         }
 
-        //Validação de email
-        if (!email.contains("@") && !email.contains(".com") && !(email == null || email.isBlank())){
+        //Validação de email,
+        if (!EmailValidator.isValid(email)){
             throw new IllegalArgumentException("Email inválido");
         }
 
@@ -18,16 +22,29 @@ public class User {
         this.email = email;
     }
 
-    public String consultEmail() {
+    public String getEmail() {
         return email;
     }
 
-    public String consultUsername() {
+    public String getUsername() {
         return username;
     }
 
-    public long calculateWorkload() {
-        consultUsername();
-        return 0; 
+    public long calculateWorkload(Workspace workspace) {
+        long countInProgress = workspace.getTasks().stream()
+        .filter(p->p.getStatus().equals(TaskStatus.IN_PROGRESS) && p.getOwner().equals(this)).count();
+        return countInProgress;
     }
+
+    public class EmailValidator {
+        private static final String EMAIL_REGEX = "^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$";
+        
+        public static boolean isValid(String email){
+            return Pattern.matches(EMAIL_REGEX, email);
+        }
+    }
+    /*private boolean verifyEmail(String email){
+        if (email == null || email.isBlank()) return 0;
+        if (email.startsWith(String))
+    }*/ //
 }
